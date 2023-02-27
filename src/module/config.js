fbd35cf4e6d7be0bc6532f7e83914440bd16cb5a
Ricky <rckruse91@gmail.com>
2023-02-27 16:45:38 -0500

    fixing Esoteric Warden breakage with last commit, removing libwrapper dependency

--- src/module/config.js
+++ src/module/config.js
@@ -5,16 +5,6 @@ Hooks.on("init", () => {
     exploitVuln,
     forceEVTarget,
   };
-  libWrapper.register(
-    "pf2e-thaum-vuln",
-    "game.pf2e.actions.restForTheNight",
-    function (wrapper, ...args) {
-      const a = args[0].actors[0];
-      a.unsetFlag("pf2e-thaum-vuln", "EWImmuneTargs");
-      wrapper(...args);
-    },
-    "WRAPPER"
-  );
 
   //game settings
   game.settings.register("pf2e-thaum-vuln", "useEVAutomation", {
