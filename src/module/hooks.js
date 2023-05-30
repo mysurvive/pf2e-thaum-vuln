f3c932067a806602ea41c6af124ab7b69d62c5fd
Ricky <rckruse91@gmail.com>
2023-05-29 23:10:59 -0400

    initial implement commit

--- src/module/hooks.js
+++ src/module/hooks.js
@@ -12,6 +12,8 @@ import { removeEWOption } from "./feats/esotericWarden.js";
 
 import { createChatCardButton } from "./utils/chatCard.js";
 
+import { manageImplements } from "./implements.js";
+
 //This is a temporary fix until a later pf2e system update. The function hooks on renderChatMessage attack-rolls
 //If the thaumaturge makes an attack-roll, the target's weakness updates with the correct amount
 //If it's not the thaumaturge that makes the attack-roll, it changes the weakness to 0
@@ -155,5 +157,30 @@ Hooks.on("deleteItem", async (item) => {
     await sa.unsetFlag("pf2e-thaum-vuln", "EVTargetID");
     await sa.unsetFlag("pf2e-thaum-vuln", "EVMode");
     await sa.unsetFlag("pf2e-thaum-vuln", "EVValue");
+  }
+});
+
+Hooks.on("renderCharacterSheetPF2e", async (_, html) => {
+  console.log("its rendered");
+  const a = canvas.tokens.controlled[0].actor;
+  if (a.items.some((i) => i.slug === "first-implement-and-esoterica")) {
+    console.log("we hit the sheet", html);
+    const inventoryList = html.find(
+      ".sheet-body .inventory-list.directory-list.inventory-pane"
+    );
+    const manageImplementButton = $(
+      `<button class="manage-implements-button">Manage Implements</button>`
+    );
+    console.log("inventory list", inventoryList);
+    inventoryList.append(
+      `<div class="inventory-header">
+    <h3 class="item-name">Thaumaturge Implements</h3></div>
+    
+    `
+    );
+    inventoryList.append(manageImplementButton);
+    manageImplementButton.off("click").on("click", function () {
+      manageImplements();
+    });
   }
 });
