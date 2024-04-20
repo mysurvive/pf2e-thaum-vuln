7537fa239cabb4621aca21113c6cb9c7e3cfd814
Ricky <rckruse91@gmail.com>
2024-04-20 13:39:42 -0400

    add weapon implement class

--- src/module/implements/impDict.js
+++ src/module/implements/impDict.js
@@ -2,11 +2,13 @@ import { Tome } from "./implementBenefits/tome";
 import { Amulet } from "./implementBenefits/amulet";
 import { Lantern } from "./implementBenefits/lantern";
 import { Implement } from "./implement";
+import { Weapon } from "./implementBenefits/weapon";
 
 const impDict = new Map([
   ["tome", Tome],
   ["amulet", Amulet],
   ["lantern", Lantern],
+  ["weapon", Weapon],
 ]);
 
 function constructChildImplement(implement, actor, item) {
