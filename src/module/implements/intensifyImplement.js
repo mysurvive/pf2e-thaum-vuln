be67408f3761bbaa435d0c67c253e02e1076fd66
Ricky <rckruse91@gmail.com>
2024-05-20 04:10:02 -0400

    Fix some errors in IV function

--- src/module/implements/intensifyImplement.js
+++ src/module/implements/intensifyImplement.js
@@ -2,6 +2,14 @@ import { hasFeat } from "../utils/helpers";
 
 async function intensifyImplement(actor) {
   actor ??= game.user.character;
+
+  // This only applies to GMs who do not select a token
+  if (!actor) {
+    return ui.notifications.warn(
+      game.i18n.localize("pf2e-thaum-vuln.notifications.warn.noToken")
+    );
+  }
+
   const imps = actor.attributes.implements;
 
   if (!hasFeat(actor, "intensify-vulnerability"))
