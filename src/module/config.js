6c5486631b4587cd9fc552c14a8315cc031fa2ec
Ricky <rckruse91@gmail.com>
2025-01-31 22:07:12 -0500

    Move EVDialog to AppV2, add CSS

--- src/module/config.js
+++ src/module/config.js
@@ -85,6 +85,32 @@ Hooks.on("init", async () => {
     if (v1.length > 0) return options.fn(this);
     else return options.inverse(this);
   });
+  Handlebars.registerHelper("when", function (op1, operator, op2, opts) {
+    const operators = {
+        eq: function (l, r) {
+          return l == r;
+        },
+        noteq: function (l, r) {
+          return l != r;
+        },
+        gt: function (l, r) {
+          return Number(l) > Number(r);
+        },
+        or: function (l, r) {
+          return l || r;
+        },
+        and: function (l, r) {
+          return l && r;
+        },
+        "%": function (l, r) {
+          return l % r === 0;
+        },
+      },
+      result = operators[operator](op1, op2);
+
+    if (result) return opts.fn(this);
+    else return opts.inverse(this);
+  });
 
   //game settings
   game.settings.register("pf2e-thaum-vuln", "0150migration", {
